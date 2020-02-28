e16f21f8a28f62749204154319dd42cfdc7238d3
Nicolas Espinosa <nespinosa@midasconsultores.com.ar>
2020-02-28 16:22:48 -0300

    comaccesig-data-rest

---
+++ src/main/java/accessingdatarest/accessingdatarest/AccessingDataRestApplication.java
@@ -0,0 +1,15 @@
+package accessingdatarest.accessingdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingDataRestApplication.class, args);
+		
+		
+	}
+
+}
